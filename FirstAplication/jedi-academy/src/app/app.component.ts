57db74134290f9ba47342a0bfe9810329a14b404
Yago Henrique <yagozt.henrique@gmail.com>
2018-08-29 14:45:22 -0300

    finalizando projeto inicial

--- FirstAplication/jedi-academy/src/app/app.component.ts
+++ FirstAplication/jedi-academy/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Student } from "./student/student.model";
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jedi-academy';
+  students: Student[] = [
+    {name: "Yago Henrique", age: 24, company: "Dacasa", dateBirthday: new Date("1994/03/22")},
+    {name: "Lucas", age: 13, dateBirthday: new Date("2005/03/22")}
+  ];
 }
---
+++ FirstAplication/jedi-academy/src/app/student/student.model.ts
@@ -0,0 +1,6 @@
+export interface Student {
+    name: string;
+    age: number;
+    company?: string;
+    dateBirthday?: Date;
+}
