c39ca35115586f1f4a44880611c35fbaf10bdff8
Yago Henrique <yagozt.henrique@gmail.com>
2018-09-03 08:42:16 -0300

    projeto

---
+++ FirstAplication/teste/src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import { RestaurantsService } from '../../restaurants/restaurants.service';
+import { Observable } from 'rxjs/Observable';
+import { MenuItem } from '../menu-item/menu-item.model';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  menuItems: Observable<MenuItem[]>;
+  
+  constructor(private restaurantService: RestaurantsService, 
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.menuItems = this.restaurantService.menuOfRestaurant(this.route.parent.snapshot.params['id']);
+  }
+
+  addMenuItem(item: MenuItem){
+    console.log(item);
+  }
+
+}
---
+++ FirstAplication/teste/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  restaurant:Restaurant;
+  constructor(private restaurantService: RestaurantsService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.restaurantService.restaurantById(this.route.snapshot.params['id'])
+                          .subscribe(restaurant => this.restaurant = restaurant);
+  }
+
+}
